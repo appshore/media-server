844a18013302d21167ea454be64f37b5f4610bd5
AppShore <git@appshore.com>
2019-07-14 22:35:38 +0100

    inital settings

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "arrow-parens": [2, 'as-needed'],
+        "curly": [2, 'all'],
+        'brace-style': [2, '1tbs', { allowSingleLine: false }],
+        'camelcase': [2, { ignoreDestructuring: true }],
+        'comma-dangle': [2, 'never'],
+        'comma-spacing': 1,
+        'implicit-arrow-linebreak': 0,
+        'no-confusing-arrow': 0,
+        'no-cond-assign': 0,
+        'no-plusplus': 0
+    }
+};
---
+++ README.md
@@ -0,0 +1,30 @@
+# Media search - server side
+
+## Installation
+
+- Node.js/Express server
+- configure port and Youtube api in .env
+
+```
+# Install dependencies
+yarn
+
+# Build server
+yarn build
+
+# Start server
+yarn start
+
+# Start server in dev mode with nodemon
+yarn dev
+
+# Debug server
+yarn debug:map
+yarn debug
+
+# Test
+yarn test
+
+# Lint
+yarn lint
+```
